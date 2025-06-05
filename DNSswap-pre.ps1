20ced6b69fb1487315f11d042709bd117659f907
sonikku10 <sonikku10@msn.com>
2025-06-05 10:07:38 -0400

    Update DNSswap-pre.ps1
    "% |" loop changed to "ForEach-Object" for clarity

--- DNSswap-pre.ps1
+++ DNSswap-pre.ps1
@@ -1,4 +1,4 @@
-#For PowerShell 5.1 on Windows Server
+# For PowerShell 5.1 on Windows Server
 
 $time = Get-Date
 
@@ -10,12 +10,12 @@ if((Test-Path <path-to-text-file>) -or (Test-Path <path-to-failover-site-text-fi
   Write-Host "Site/Application in FAILOVER STATUS."
 
   #Pre-Script: Restore to Primary Site
-  $targetServers | %{ Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <forward_dnsZone> -RRType "A" -Name "$_" -Confirm:$false -Force }
-  $lastOctets | %{ Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <secondary_reverse_dnsZone> -RRType "PTR" -Name "$_" -Confirm:$false -Force }
+  $targetServers | ForEach-Object -Process { Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <forwardDnsZone> -RRType "A" -Name "$_" -Confirm:$false -Force }
+  $lastOctets | ForEach-Object -Process { Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <secondary_reverseDnsZone> -RRType "PTR" -Name "$_" -Confirm:$false -Force }
 } 
 else {
   #Pre-Script: Recover to Secondary Site
   Write-Host "Site/Application is not yet in Failover Status."
-  $targetServers | %{ Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <forward_dnsZone> -RRType "A" -Name "$_" -Confirm:$false -Force }
-  $lastOctets | %{ Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <primary_reverse_dnsZone> -RRType "PTR" -Name "$_" -Confirm:$false -Force }
+  $targetServers | ForEach-Object -Process { Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <forwardDnsZone> -RRType "A" -Name "$_" -Confirm:$false -Force }
+  $lastOctets | ForEach-Object -Process { Remove-DnsServerResourceRecord -ComputerName $dnsServer -ZoneName <primary_reverseDnsZone> -RRType "PTR" -Name "$_" -Confirm:$false -Force }
 }
